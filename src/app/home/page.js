32fcb6b327e22a2bc24e002232bf3cd778043aec
Nicolas Moreno <nicolas007andres@hotmail.com>
2023-07-31 12:48:19 -0500

    cambio raiz del proyecto

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,6 +1,6 @@
 import Layout from '@/components/Layout'
 import Image from 'next/image'
-import ProfilePicture from '../../public/Image/Profile/ProfilePicture.PNG'
+import ProfilePicture from '../../../public/Image/Profile/ProfilePicture.PNG'
 import AnimatedText from '@/components/AnimatedText'
 import Link from 'next/link'
 import { LinkArrow } from '@/components/Icons'
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,7 +30,7 @@ const Navbar = () => {
   return (
     <header className='w-full px-32 py-8 font-medium flex items-center justify-between'>
       <nav>
-        <CustomLink href="/" title="Home" className='mr-4 ' />
+        <CustomLink href="/home" title="Home" className='mr-4 ' />
         <CustomLink href="/about" title="About" className='mx-4' />
         {/* <CustomLink href="/projects" title="Projects" className='mx-4' />
         <CustomLink href="/articles" title="Articles" className='ml-4' /> */}
