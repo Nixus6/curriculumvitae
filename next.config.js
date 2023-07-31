4754d1bb9d2233de4a988f58866d638a32f5d2a9
Nicolas Moreno <53442377+Nixus6@users.noreply.github.com>
2023-07-31 12:55:01 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    //output: 'export',
+    output: 'export',
     reactStrictMode: true,
-    async redirects() {
-        return [
-          {
-            source: '/',
-            destination: '/home',
-            permanent: true,
-          },
-        ]
-      },
 }
 
 module.exports = nextConfig
